e5f556c1085f977f6720a65f1637663ee7297747
Karim Ahmed <kareem1999@gmail.com>
2020-01-24 17:04:35 +0200

    Added list of devices using database, 2 screens will be merged soon

--- app/src/main/java/com/example/tempstuff/SearchForDevices.kt
+++ app/src/main/java/com/example/tempstuff/SearchForDevices.kt
@@ -2,122 +2,105 @@ package com.example.tempstuff
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.FirebaseFirestore
+import android.provider.ContactsContract.CommonDataKinds.Note
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.google.firebase.firestore.Query
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+
+
 const val EXTRA_MESSAGE = "com.example.karim.tempstuff.MESSAGE"
 
 class SearchForDevices : AppCompatActivity() {
-    private val mylist = ArrayList<MyList>()
-    private var recyclerView: RecyclerView? = null
-    private var mAdapter: ListAdapter? = null
+    private val db = FirebaseFirestore.getInstance()
+    private val DevicesRef = db.collection("Devices")
+
+    private var adapter: ListAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search_device)
 
-        recyclerView = findViewById(R.id.recycler_view) as RecyclerView
-
-        mAdapter = ListAdapter(mylist)
-        val mLayoutManager = LinearLayoutManager(applicationContext)
-        recyclerView!!.layoutManager = mLayoutManager
-        recyclerView!!.itemAnimator = DefaultItemAnimator()
-        recyclerView!!.adapter = mAdapter
-
-
-        setSupportActionBar(findViewById(R.id.toolbar))
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setHomeButtonEnabled(true)
-        supportActionBar?.setTitle("")
-        preparelistData()
+        setUpRecyclerView()
     }
 
-    private fun preparelistData() {
-        var item = MyList("A","B","C")
-        mylist.add(item)
-        item = MyList("B","C","D")
-        mylist.add(item)
+    private fun setUpRecyclerView() {
+        val query = DevicesRef.orderBy("Device ID", Query.Direction.DESCENDING)
 
-        mAdapter!!.notifyDataSetChanged()
+        val options = FirestoreRecyclerOptions.Builder<MyList>()
+            .setQuery(query, MyList::class.java)
+            .build()
+
+        adapter = ListAdapter(options)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view) as RecyclerView
+        recyclerView.setHasFixedSize(true)
+        recyclerView.setLayoutManager(LinearLayoutManager(this))
+        recyclerView.setAdapter(adapter)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter!!.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter!!.stopListening()
     }
 }
 
 //The class for the items in the list to be added, can be expanded
 class MyList {
-    var DeviceNameText: String = ""
-    var DeviceTypeText: String = ""
-    var DeviceModelText: String = ""
+    var Name: String = ""
+    var Type: String = ""
+    var Model: String = ""
     constructor() {}
-    constructor(DeviceName: String , DeviceType: String, DeviceModel: String) {
-        this.DeviceNameText = DeviceName
-        this.DeviceTypeText = DeviceType
-        this.DeviceModelText= DeviceModel
+    constructor(DName: String , DType: String, DModel: String) {
+        this.Name = DName
+        this.Type = DType
+        this.Model= DModel
     }
 
-    fun getName(): String {
-        return DeviceNameText
-    }
 
-    fun setName(Name: String) {
-        this.DeviceNameText = Name
-    }
-    fun getType(): String {
-        return DeviceTypeText
-    }
-
-    fun setType(Type: String) {
-        this.DeviceTypeText = Type
-    }
-    fun getModel(): String {
-        return DeviceModelText
-    }
-
-    fun setModel(Model: String) {
-        this.DeviceModelText = Model
-    }
 
 }
-class ListAdapter(var theList: List<MyList>) :
-    RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+class ListAdapter(options: FirestoreRecyclerOptions<MyList>) :
+    FirestoreRecyclerAdapter<MyList, ListAdapter.MyListHolder>(options) {
 
-    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        var theType: TextView
-        var theButton: Button
+    override fun onBindViewHolder(holder: MyListHolder, position: Int, model: MyList) {
+        holder.textDeviceName.setText(model.Name)
+        holder.textDeviceType.setText(model.Type)
+        holder.textDeviceModel.setText(model.Model)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyListHolder {
+        val v = LayoutInflater.from(parent.context).inflate(
+            R.layout.list_name_button,
+            parent, false
+        )
+        return MyListHolder(v)
+    }
+
+    inner class MyListHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var textDeviceName: TextView
+        var textDeviceType: TextView
+        var textDeviceModel: TextView
+
         init {
-            theType = view.findViewById(R.id.DeviceTypeView)
-            theButton = view.findViewById(R.id.button2)
+            textDeviceName = itemView.findViewById(R.id.DeviceTypeView)
+            textDeviceType = itemView.findViewById(R.id.DeviceNameView)
+            textDeviceModel = itemView.findViewById(R.id.DeviceModelView)
         }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.list_name_button, parent, false)
-
-        return MyViewHolder(itemView)
-    }
-
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val device = theList[position]
-        holder.theType.setText(device.getType())
-
-        //holder.theButton.setOnClickListener {
-            //lateinit var context : Context
-            //var message = device.getName()
-            //var intent= Intent(context, TestScreen::class.java).
-                //putExtra(EXTRA_MESSAGE, message)
-            //startActivity(context, intent, null)
-        //}
-    }
-
-    override fun getItemCount(): Int {
-        return theList.size
     }
 }
