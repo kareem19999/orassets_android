14ba18873422f78d676e5c6f2ead118e1138e170
Karim Ahmed <kareem1999@gmail.com>
2020-01-24 13:20:05 +0200

    Added functioninng QR scanner, but room for improvement

--- app/src/main/java/com/example/tempstuff/ScanActivity.kt
+++ app/src/main/java/com/example/tempstuff/ScanActivity.kt
@@ -6,40 +6,72 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.zxing.Result
 import me.dm7.barcodescanner.zxing.ZXingScannerView
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.content.Intent
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
-class ScanActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
 
-    private var mScannerView: ZXingScannerView? = null
+class ScanActivity : AppCompatActivity(), View.OnClickListener {
 
-    public override fun onCreate(state: Bundle?) {
-        super.onCreate(state)
-        mScannerView = ZXingScannerView(this)   // Programmatically initialize the scanner view
-        setContentView(mScannerView)                // Set the scanner view as the content view
+    //View Objects
+    private var buttonScan: Button? = null
+    private var textViewName: TextView? = null
+    private var textViewAddress: TextView? = null
+
+    //qr code scanner object
+    private var qrScan: IntentIntegrator? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan)
+
+        //View objects
+        buttonScan = findViewById(R.id.buttonScan) as Button
+        textViewName = findViewById(R.id.textViewID) as TextView
+
+        //intializing scan object
+        qrScan = IntentIntegrator(this)
+
+        //attaching onclick listener
+        buttonScan!!.setOnClickListener(this)
     }
 
-    public override fun onResume() {
-        super.onResume()
-        mScannerView!!.setResultHandler(this) // Register ourselves as a handler for scan results.
-        mScannerView!!.startCamera()          // Start camera on resume
+    //Getting the scan results
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            //if qrcode has nothing in it
+            if (result.contents == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show()
+            } else {
+                //if qr contains data
+                try {
+                    //converting the data to json
+                    val obj = JSONObject(result.contents)
+                    //setting values to textviews
+                    textViewName!!.text = obj.getString("name")
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                    //if control comes here
+                    //that means the encoded format not matches
+                    //in this case you can display whatever data is available on the qrcode
+                    //to a toast
+                    Toast.makeText(this, result.contents, Toast.LENGTH_LONG).show()
+                }
+
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 
-    public override fun onPause() {
-        super.onPause()
-        mScannerView!!.stopCamera()           // Stop camera on pause
+    override fun onClick(view: View) {
+        //initiating the qr code scan
+        qrScan!!.initiateScan()
     }
-
-    override fun handleResult(rawResult: Result) {
-        // Do something with the result here
-        Log.v("tag", rawResult.getText()); // Prints scan results
-        Log.v("tag", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
-
-        MainActivity.tvresult!!.setText(rawResult.text)
-        var tvresult= rawResult.getText();
-        Toast.makeText(this, tvresult,Toast.LENGTH_LONG).show();
-        onBackPressed()
-
-        // If you would like to resume scanning, call this method below:
-        //mScannerView.resumeCameraPreview(this);
-    }
-
 }
